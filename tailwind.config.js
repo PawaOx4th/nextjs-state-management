f3accf8425cc38a0af2d7591b3e6aadcb9079cf7
pawaox4th <pawaox4th@gmail.com>
2022-04-13 14:14:14 +0700

    feat: 🎸 Create config for tailwind.config.js and postcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./*.{js,ts.jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: "Inter, sans-serif"
+      }
+    }
+  },
+  plugins: []
+}
